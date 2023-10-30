faac9920568ce9d3577c51feb2fef41d74b1f006
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 19:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let elemet = document.getElementById("level");
+let level = 0;
+while(element.parentNode){
+	level++;
+	element = elemet.parentNode;
+}
+alert("The level of the element is: ${level}");
